36e32c4fe373b9039addb350d9ffc8a59974774e
Stas <nevmyvakastas@gmail.com>
2020-09-29 23:43:05 +0600

    wrote simple commands

--- index.js
+++ index.js
@@ -24,18 +24,35 @@ bot.onText(/remindme (.+) at (.+)/, function (msg, match) {
     bot.sendMessage(userId, 'The dark side will devour those who lack the power to control it.')
 });
 
-setInterval(function(){
-    for (var i = 0; i < notes.length; i++) {
-    const curDate = new Date().getHours() + ':' + new Date().getMinutes()
-    if (notes[i]['time'] === curDate) {
-      bot.sendMessage(notes[i]['uid'], 'Remind you, you should: '+ notes[i]['text'] + ' now for the dark side.')
-      notes.splice(i, 1)
-    }
-  }
-}, 1000);
+bot.on('message', (msg) => {
 
-bot.on('message', function (msg) {
-    var chatId = msg.chat.id // get chat Id
-    var photo = 'YodaSith.jpg';
-    bot.sendPhoto(chatId, photo, { caption: 'Sith Yoda' })
+  var hi = "hi";
+  if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+  bot.sendMessage(msg.chat.id,"Hi Sith");
+  } 
+      
+  var bye = "bye";
+  if (msg.text.toString().toLowerCase().includes(bye)) {
+  bot.sendMessage(msg.chat.id, "Bye Sith");
+  }
 });
+
+bot.onText(/\/showforce/, (msg) => {
+  bot.sendMessage(msg.from.id, "Hmm, hmm... I see your name. You are " + msg.from.first_name);
+});
+
+bot.onText(/\/showyourself/, (msg) => {
+  var chatId = msg.chat.id // get chat Id
+      var photo = 'YodaSith.jpg';
+      bot.sendPhoto(chatId, photo, { caption: "I'm here" }) 
+});
+
+setInterval(function(){
+  for (var i = 0; i < notes.length; i++) {
+  const curDate = new Date().getHours() + ':' + new Date().getMinutes()
+  if (notes[i]['time'] === curDate) {
+    bot.sendMessage(notes[i]['uid'], 'Remind you, you should: '+ notes[i]['text'] + ' now for the dark side.')
+    notes.splice(i, 1)
+  }
+}
+}, 1000);
